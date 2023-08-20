15d2004fa17c0e9c4d555e004775895254dfa5d1
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-20 14:19:39 +0400

    Update

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,17 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius) {
-        int fahrenheitInteger = (temperatureCelsius * 9) / 5 + 32;
-        int fahrenheitDecimal = ((temperatureCelsius * 9) % 5) * 10 / 5;
-
-        System.out.print(fahrenheitInteger);
-        System.out.print('.');
-        System.out.print(fahrenheitDecimal);
+        int temperatureFahrenheit = (temperatureCelsius * 9 / 5) + 32;
+        System.out.println(temperatureFahrenheit);
     }
 
     public static void main(String[] args) {
         TemperatureConverter converter = new TemperatureConverter();
-        converter.toFahrenheit(1); // Output will be "33.8"
+        converter.toFahrenheit(0); // Output will be "32"
     }
 }
 
